bd350a1faf5e51f65e9b77f4c26d52118f942c3a
jasa1aw <erakrut05@gmail.com>
2024-02-26 23:11:49 +0600

    authorization with nextauth.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,24 @@
-
+'use client'
 import styles from "./page.module.css";
 import Dashboard from "./dashboard/page";
 import Header from '@/components/header/header';
 import SlideMenu from "@/components/SlideMenu/slideMenu";
+import Login from '@/components/Login/login';
+import { useSession } from "next-auth/react";
 
 export default function Home() {
+  const {data: session} = useSession();
   return (
     <main className={styles.main}>
       <Header/>
-      <SlideMenu/>
-      <Dashboard/>
+      {session && (
+        <>
+          <SlideMenu/>
+          <Dashboard/>
+        </> 
+      )}
+      
+      <Login/>
     </main>
   );
 }
---
+++ src/components/Login/login.tsx
@@ -0,0 +1,17 @@
+'use client'
+import { useSession, signIn, signOut } from "next-auth/react"
+import Button from "@mui/material/Button";
+
+export default function Login(){
+    const { data: session } = useSession();
+    if(session) {
+        return <>
+            
+            <Button variant={'contained'} color={'error'} onClick={() => signOut()}>Sign out</Button>
+        </>
+    }
+    return <>
+        <h2>Please log in</h2> <br/>
+        <Button variant={'contained'} color={'success'} onClick={() => signIn()}>Sign in</Button>
+    </>
+}
